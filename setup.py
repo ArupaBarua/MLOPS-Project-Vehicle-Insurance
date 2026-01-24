083151d679adad3d71cff65fd96876520eb2ef10
Arupa <arupab362@gmail.com>
2026-01-24 20:09:37 +0600

    Project structure ready and venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    auhtor="Arupa Barua",
+    author_email="arupab362@gmail.com",
+    packages=find_packages()
+)
